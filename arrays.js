b7456c6daa4898441f1e66c93e4f6c69d6e05960
Milind Mishra <milind.mishra4@gmail.com>
2021-10-17 00:50:03 +0530

    updates

---
+++ arrays.js
@@ -0,0 +1,17 @@
+// arrays declaration
+// let arr = [];
+let array = [1, 2, 3, 4, 5];
+console.log(array);
+// console.log(array.length);
+// let i = 0;
+// while(i < array.length) {
+//     console.log("element at index ", i, " is ", array[i]);
+//     i++;
+// }
+// push, unshift
+array.push("last value"); // add to the end of the array
+array.unshift("first value"); // add to the beginning of the array
+console.log(array);
+array.shift(); // removes first element
+array.pop(); // removes last element
+console.log(array);
