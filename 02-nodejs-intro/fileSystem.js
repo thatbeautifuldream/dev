bb4def985fc72ecddc57c47786be08affd7a1293
Milind Mishra <milind.mishra4@gmail.com>
2021-10-17 03:33:49 +0530

    updates

---
+++ 02-nodejs-intro/fileSystem.js
@@ -0,0 +1,8 @@
+// fileSystem
+// files -> create, read, update, delete
+let fs = require("fs");
+// let buffer = fs.readFileSync("abc.js");
+// console.log("bin data " + buffer);
+// create
+// fs.openSync("abc.txt", "w");
+fs.writeFileSync("abc.txt", "This is a text file.");
--- 02-nodejs-intro/os.js
+++ 02-nodejs-intro/os.js
@@ -2,4 +2,5 @@
 let os = require("os");
 // console.log(os.arch()); // x64
 // console.log(os.platform()); // win32
-// console.log(os.networkInterfaces()); // {en0: [{…}], en1: [{…}], …}
+// console.log(os.networkInterfaces());
+// console.log(os.cpus());
