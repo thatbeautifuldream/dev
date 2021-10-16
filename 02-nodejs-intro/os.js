19623141b7bad008e45d70a87e0482d1ac23fea8
Milind Mishra <milind.mishra4@gmail.com>
2021-10-17 02:31:47 +0530

    os

---
+++ 02-nodejs-intro/os.js
@@ -0,0 +1,5 @@
+// os features
+let os = require("os");
+// console.log(os.arch()); // x64
+// console.log(os.platform()); // win32
+// console.log(os.networkInterfaces()); // {en0: [{…}], en1: [{…}], …}
