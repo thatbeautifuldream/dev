45b916cd1a5999b9e2f281c231c379a91c908ac6
Milind Mishra <milind.mishra4@gmail.com>
2021-10-21 00:09:28 +0530

    file system

--- 02-nodejs-intro/fileSystem.js
+++ 02-nodejs-intro/fileSystem.js
@@ -1,10 +1,39 @@
 // fileSystem
-// files -> create, read, update, delete
+// files -> create, read,               update,          delete
+// open -w          readfileSync        appendfile       unlinkSync
+// writefileSync
+
 let fs = require("fs");
 // let buffer = fs.readFileSync("abc.js");
 // console.log("bin data " + buffer);
 // create
 // fs.openSync("abc.txt", "w");
-fs.writeFileSync("abc.txt", "This is a text file.");
+// no file -> create and file exist -> content replaced
+// create write
+// fs.writeFileSync("abc.txt", "This is a text file.");
 // update
-fs.appendFileSync("abc.txt", "Are you happy now ?");
+// fs.appendFileSync("abc.txt", "Are you happy now ?");
+
+// folder
+// create       read                     delete
+// mkdirSync    readdirSync             rmdirSync
+// fs.mkdirSync("myDirectory");
+
+// fs.writeFileSync("myDirectory/myFile.txt", "My content");
+// let content = fs.readFileSync("myDirectory"); // all files listed inside directory
+// console.log(content);
+// for (let i = 0; i < content.length; i++) {
+//   console.log("file " + content[i] + " is removed");
+//   // remove file
+//   fs.unlinkSync("myDirectory/" + content[i]);
+// }
+// removes folder
+// fs.rmdirSync("myDirectory");
+// fs.exixstsSync("myDirectory");-> true or false
+let doesPathExist = fs.existsSync("myDirectory");
+console.log(doesPathExist);
+// fs.lstatSync("myDirectory"); -> stats
+let detailsObj = fs.lstatSync(__dirname + "\\fileSystem.js");
+let ans = detailsObj.isFile();
+console.log(ans);
+ans = detailsObj.isDirectory();
