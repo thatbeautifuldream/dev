d8b1d0d49d680d130145a982e564d22f19808430
Milind Mishra <milind.mishra4@gmail.com>
2021-10-23 02:37:27 +0530

    updates

--- 03-file-system-organizer/input.js
+++
@@ -1,3 +0,0 @@
-// taking input in node js
-let inputArr = process.argv.slice(2);
-console.log(inputArr);
---
+++ 03-file-system-organizer/main.js
@@ -0,0 +1,6 @@
+// taking input in node js
+let inputArr = process.argv.slice(2);
+console.log(inputArr);
+// node main.js tree "directoryPath"
+// node main.js organise "directoryPath"
+// node main.js help
