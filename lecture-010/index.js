bb47dfed34956313a23d597cf7081f97ae5d1896
Milind Mishra <milind.mishra4@gmail.com>
2022-01-08 21:38:43 +0530

    new

--- lecture-010/index.js
+++ lecture-010/index.js
@@ -1,1 +1,6 @@
 // !Agenda for today:
+// Node.js
+// Copying Files and Directories using fs module
+// importing the required modules
+const fs = require("fs"); // fs module : file system
+const path = require("path"); // path module : to get the path of the file
