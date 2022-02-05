9fb08478f3113bf11048a9762aa6274e0937e0da
Milind Mishra <milind.mishra4@gmail.com>
2022-02-05 22:45:54 +0530

    📦 NEW: add

--- lecture-019/undefined.js
+++ lecture-019/undefined.js
@@ -1,23 +1,25 @@
-console.log(a); // but it is undefined, since it is not valued yet
+const log = console.log;
+
+log(a); // but it is undefined, since it is not valued yet
 var a = 2; // gets memory location
 
 var x;
-console.log(x); // undefined
+log(x); // undefined
 
 function test() {
   // returns nothing
 }
 
-console.log(test()); // undefined
+log(test()); // undefined
 
 function test2() {
   return null; // explicitly assigned null
 }
 
-console.log(test2()); // returns null
+log(test2()); // returns null
 
 let b = global.x; // using a global objects x which is non existent
-console.log(b); // undefined
+log(b); // undefined
 
 let person = {
   name: "Adam",
@@ -28,7 +30,7 @@ let p1 = person.name; // p1 is a string
 let p2 = person.age; // p2 is a number
 let p3 = person.address; // p3 is undefined
 
-console.log(`${p1} is ${p2} years old from ${p3}`);
+log(`${p1} is ${p2} years old from ${p3}`);
 // Adam is 30 years old from undefined
 
 // whenever we dont assign value to a variable, it is undefined
@@ -37,3 +39,11 @@ console.log(`${p1} is ${p2} years old from ${p3}`);
 // Null vs undefined
 // Null is a value that is explicitly assigned
 // undefined is a value that is not assigned
+
+let formObj = {
+  firstName: "Milind",
+  middleName: null, // explicitly assigned null
+  lastName: "Mishra",
+};
+
+log(formObj.middleName); // null
